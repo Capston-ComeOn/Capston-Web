a2bdfede2c9f2143c0fd4354b0c5d8c92f321c2f
kimnoin <kimnoin7@tistory.com>
2019-09-10 21:36:26 +0900

    [front] vue cli 3.x & vuetify 설치

--- frontEnd/src/App.vue
+++ frontEnd/src/App.vue
@@ -1,24 +1,36 @@
 <template>
-  <div id="app">
-    <Navbar/>
-    <router-view/>
-    <Footer/>
-  </div>
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">MATERIAL DESIGN</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        text
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Latest Release</span>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-  import Navbar from './components/Navbar'
-  import Footer from './components/Footer'
+import HelloWorld from './components/HelloWorld';
 
-  export default {
-    components: {
-      Navbar,
-      Footer
-    },
-    name: 'App'
-  }
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+  data: () => ({
+    //
+  }),
+};
 </script>
-
-<style>
-
-</style>
