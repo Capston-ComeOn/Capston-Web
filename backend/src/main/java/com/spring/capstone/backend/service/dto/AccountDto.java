3c748875f3b2d979aecb645797867aaca983d5d9
imkimheejoo <jujudamdadi@naver.com>
2019-09-15 20:24:21 +0900

    chore: 필요없는 메서드 제거

--- backend/src/main/java/com/spring/capstone/backend/service/dto/AccountDto.java
+++ backend/src/main/java/com/spring/capstone/backend/service/dto/AccountDto.java
@@ -1,10 +1,8 @@
 package com.spring.capstone.backend.service.dto;
 
-import com.spring.capstone.backend.domain.accounts.AccountRoles;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotBlank;
-import java.util.Set;
 
 public class AccountDto {
 
@@ -14,7 +12,6 @@ public class AccountDto {
     private String email;
     @Length(min = 8, max = 12)
     private String password;
-    private Set<AccountRoles> roles;
 
     public AccountDto(String name, String email, String password) {
         this.name = name;
@@ -34,11 +31,4 @@ public class AccountDto {
         return name;
     }
 
-    public Set<AccountRoles> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(Set<AccountRoles> roles) {
-        this.roles = roles;
-    }
 }
